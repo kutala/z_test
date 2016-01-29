84ddf318be7c36c43962e09e21203a19f5b7e4d7
kutala <80011355@qq.com>
2016-01-29 15:09:35 +0800

    add main method body

--- src/z_test/Brach.java
+++ src/z_test/Brach.java
@@ -3,7 +3,8 @@ package z_test;
 public class Brach {
 
     public static void main(String[] args) {
-
+        System.out.println("main");
+        System.out.println("main");
     }
 
     public Brach() {}
